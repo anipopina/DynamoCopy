7cebf6af018e1b7ca445451d2b7fabc950502ed6
Ryosuke Komiyama <Ryon@Ryons-MacBook-2016.local>
2016-08-10 17:33:44 +0900

    fix

--- DynamoCopy/DynamoCopy.js
+++ DynamoCopy/DynamoCopy.js
@@ -1,9 +1,3 @@
-// consoleやalertの使用を許可
-/*jslint devel: true*/
-// ++の使用を許可
-/*jslint plusplus: true*/
-// 自由なインデントを許可
-/*jslint white: true*/
 // 他のファイルで定義済みのオブジェクトの使用を許可
 /*global
 	require: false,
@@ -24,7 +18,7 @@ exports.handler = function (event, context) {
 	var sourceTableName = event.source_table_name;
 	var destTableName = event.dest_table_name;
 	// functions
-	var main = null;
+	var main;
 	var getSourceData;
 	var writeData;
 
